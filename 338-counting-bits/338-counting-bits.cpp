2eeedd4925021426a71798274ee63fb53d717751
Harshit Gupta <me2190885@iitd.ac.in>
2022-06-08 02:18:58 +0530

    Time: 7 ms (65.51%), Space: 8.5 MB (21.00%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    int count(int i){
-        int a = 0;
-        while(i){
-            a+=i%2;
-            i/=2;
-        }
-        return a;
-    }
     vector<int> countBits(int n) {
         vector<int> ans = {0};
         for(int i= 1;i<=n;i++){
