a2977c520b30ff5d6f6b268e900b88005953325d
Harshit Gupta <me2190885@iitd.ac.in>
2022-06-16 15:36:06 +0530

    Time: 47 ms (14.32%), Space: 5.9 MB (72.90%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0)return false;
+        if(n==1)return true;
+        else if(n%3==0)return isPowerOfThree(n/3);
+        else return false;
+    }
+};
