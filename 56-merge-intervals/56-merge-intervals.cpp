5297b091267d79fc60b03c1a7958138f91c03b62
Harshit Gupta <me2190885@iitd.ac.in>
2022-06-21 23:53:44 +0530

    Time: 438 ms (5.02%), Space: 47.1 MB (6.35%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),[](vector<int> a, vector<int> b){
+            return a[0]<b[0];
+            });
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        for(int i = 1;i<intervals.size();i++){
+            if(ans.back()[1]<intervals[i][0])ans.push_back(intervals[i]);
+            else if(ans.back()[1]==intervals[i][0])ans.back()[1] = intervals[i][1];
+            else{
+                if(ans.back()[1]<intervals[i][1])ans.back()[1] = intervals[i][1];
+            }
+        }
+        return ans;
+    }
+};
