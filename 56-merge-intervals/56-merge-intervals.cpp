e7767910e8fc50212d00f7cb391491cfe050150e
Harshit Gupta <me2190885@iitd.ac.in>
2022-06-21 23:56:24 +0530

    Time: 53 ms (55.48%), Space: 19.1 MB (46.05%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),[](vector<int> a, vector<int> b){
+        sort(intervals.begin(),intervals.end(),[](vector<int> &a, vector<int> &b){
             return a[0]<b[0];
             });
         vector<vector<int>> ans;
