bbbe9cd9a75fe5fc5cb6d2a36883dc5f00ade571
Harshit Gupta <me2190885@iitd.ac.in>
2022-06-29 01:24:29 +0530

    Time: 33 ms (99.86%), Space: 15 MB (87.97%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,27 +1,24 @@
 class Solution {
+    
 public:
-    vector<vector<int>> max_prev;
-    int n,m;
-    int maxi = 0;
-    void dfs(int dis,int i, int j, vector<vector<int>>& matrix){
-        maxi = max(maxi,dis);
-        if(dis>max_prev[i][j]){
-            max_prev[i][j] = dis;
-            if(i<n-1 && matrix[i+1][j]>matrix[i][j])dfs(dis+1,i+1,j,matrix);
-            if(i>0 && matrix[i-1][j]>matrix[i][j])dfs(dis+1,i-1,j,matrix);
-            if(j>0 && matrix[i][j-1]>matrix[i][j])dfs(dis+1,i,j-1,matrix);
-            if(j<m-1 && matrix[i][j+1]>matrix[i][j])dfs(dis+1,i,j+1,matrix);
-        }
+    int memo[200][200];
+    int longestIncreasingPath(vector<vector<int>>& M) {
+        int ylen = M.size(), xlen = M[0].size(), ans = 0;
+        for (int i = 0; i < ylen; i++)
+            for (int j = 0; j < xlen; j++)
+                ans = max(ans, dfs(i,j,M));
+        return ans;
     }
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        n = matrix.size();
-        m = matrix[0].size();
-        max_prev = vector<vector<int>>(n,vector<int>(m,0));
-        for(int i =0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                dfs(1,i,j,matrix);
-            }
-        }
-        return maxi;
+    int dfs(int y, int x, vector<vector<int>>& M) {
+        if (memo[y][x]) return memo[y][x];
+        int val = M[y][x];
+        memo[y][x] = 1 + max(
+            max(y < M.size() - 1 && M[y+1][x] < val ? dfs(y+1,x,M) : 0,
+                y > 0 && M[y-1][x] < val ? dfs(y-1,x,M) : 0),
+            max(x < M[0].size() - 1 && M[y][x+1] < val ? dfs(y,x+1,M) : 0,
+                x > 0 && M[y][x-1] < val ? dfs(y,x-1,M) : 0));
+        return memo[y][x];
+    
     }
+
 };
