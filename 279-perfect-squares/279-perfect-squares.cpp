1d88126f191b1e9dcc2a047adc2221f2a10ed054
Harshit Gupta <me2190885@iitd.ac.in>
2022-06-30 23:12:02 +0530

    Time: 321 ms (39.08%), Space: 9.3 MB (38.84%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        int m = sqrt(n)+1;
+        vector<int> sq;
+        vector<int> dp(m*m+1,0);
+        for(int i = 1;i<=m;i++){
+            sq.push_back(i*i);
+        }
+        for(int i= 1;i<=n;i++){
+            int mini = INT_MAX;
+            for(int j = 0;j<sq.size();j++){
+                if(sq[j]>i)break;
+                mini = min(mini, dp[i-sq[j]]+1);
+                
+            }
+            dp[i] = mini;
+        }
+        return dp[n];
+    }
+};
