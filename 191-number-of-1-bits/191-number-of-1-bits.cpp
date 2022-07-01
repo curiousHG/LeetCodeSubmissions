c154a88aaf4cb04b150af0bbb2237ad23bf374b7
Harshit Gupta <me2190885@iitd.ac.in>
2022-07-02 03:37:28 +0530

    Time: 0 ms (100.00%), Space: 6 MB (47.61%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int c = 0;
-        while(n){
-            c+=n%2;
-            n/=2;
-        }
-        return c;
-        
+      int ans = 0;
+      while(n!=0){
+          ans+=n%2;
+          n/=2;
+      }
+        return ans;
     }
 };
