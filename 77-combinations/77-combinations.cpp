adf58af71a49f5d7831dde68d15774966c4b10a4
Harshit Gupta <me2190885@iitd.ac.in>
2022-07-03 02:07:26 +0530

    Time: 884 ms (6.41%), Space: 165.1 MB (8.31%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> a;
+        solve(a,0,1,k,n);
+        return ans;
+    }
+    void solve(vector<int> temp,int i,int j,int k,int n){
+        if(i==k){
+            ans.push_back(temp);
+            return;
+        }
+        for(int l =j;l<=n;l++){
+            temp.push_back(l);
+            solve(temp, i+1,l+1,k,n);
+            temp.pop_back();
+        }
+    }
+};
