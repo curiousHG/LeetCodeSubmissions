769f02d6c1b122c68595a7ad57c8407fd24d2eb9
Harshit Gupta <me2190885@iitd.ac.in>
2022-07-14 17:54:44 +0530

    Time: 55 ms (57.36%), Space: 19.7 MB (75.36%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,16 +3,17 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
         unordered_map<string, vector<string>> m;
+        string t;
         for(int i = 0;i<strs.size();i++){
-            string t = strs[i];
+            t = strs[i];
             sort(t.begin(),t.end());
             if(m.find(t)==m.end()){
                 m[t] = vector<string>();
             }
             m[t].push_back(strs[i]);
         }
-        for(auto it = m.begin();it!=m.end();++it){
-            ans.push_back(it->second);
+        for(const auto& it:m){
+            ans.push_back(it.second);
         }
         return ans;
         
