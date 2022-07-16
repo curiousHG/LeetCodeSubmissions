13652230c5500ff8a070576e8e764d0a2b325745
Harshit Gupta <me2190885@iitd.ac.in>
2022-07-16 16:18:21 +0530

    Time: 55 ms (91.48%), Space: 17.3 MB (77.92%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -5,11 +5,9 @@ public:
         for(char c:s){
             a[c-'a']++;
         }
-        // for(int i:a)cout<<i<<" ";
         sort(a, a+26);
         int ans = 0;
         for(int j = 24;j>=0;j--){
-            
             if(a[j]==0)break;
             if(a[j+1]<a[j]){
                 ans+=a[j]-a[j+1];
@@ -19,8 +17,6 @@ public:
                 a[j]--;
                 ans++;
             }
-            // for(int i:a)cout<<i<<" ";
-            cout<<endl;
         }
         return ans;
     }
