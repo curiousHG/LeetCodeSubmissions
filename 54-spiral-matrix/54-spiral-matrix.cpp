4c63a8a9d6bb4351ca076cdba52937e202f285d4
Harshit Gupta <me2190885@iitd.ac.in>
2022-07-17 14:39:23 +0530

    Time: 0 ms (100.00%), Space: 7 MB (30.15%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        int n = mat.size(),m = mat[0].size();
+        vector<int> ans;
+        int i = 0,j = 0,r = m-1,d = n-1;
+        while(j<m){
+            ans.push_back(mat[i][j]);
+            cout<<i<<" "<<j<<endl;
+            j++;
+        }
+        j--;
+        i++;
+        int ki = 0,kj = 0,diri = 1,dirj = -1;
+        while(r>=0 && d>0){
+            while(ki<d){
+                ans.push_back(mat[i][j]);
+                cout<<i<<" "<<j<<" "<<d<<" "<<r<<endl;
+                i+=diri;
+                ki++;
+            }
+            i-=diri;
+            j+=dirj;
+            while(kj<r){
+                ans.push_back(mat[i][j]);
+                cout<<i<<" "<<j<<" "<<d<<" "<<r<<endl;
+                j+=dirj;
+                kj++;
+            }
+            j-=dirj;
+            i-=diri;
+            ki=0;kj =0;
+            r--;d--;
+            diri*=-1;
+            dirj*=-1;
+            
+        }
+        
+        return ans;
+        
+        
+    }
+};
