7c9537166119124ae83fc50c2af8c53949d9501a
Harshit Gupta <me2190885@iitd.ac.in>
2022-07-20 20:51:40 +0530

    Time: 3 ms (59.15%), Space: 5.9 MB (25.55%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0)return false;
+        if(__builtin_popcount(n)==1)return true;
+        return false;
+        
+    }
+};
