0ffe663f999d41b106528a6b07244cca07689ed7
Harshit Gupta <me2190885@iitd.ac.in>
2022-07-22 15:09:42 +0530

    Time: 470 ms (16.69%), Space: 134.4 MB (8.42%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        ios::sync_with_stdio(0);
         unordered_set<int> s;
         for(int i= 0;i<nums.size();i++){
             s.insert(nums[i]);
         }
+        
         int i = 1;
         while(s.find(i)!=s.end())i++;
         return i;
