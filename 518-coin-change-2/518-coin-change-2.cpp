db6dfd4922296bc4d99ede594d3caa6bccff6397
Harshit Gupta <me2190885@iitd.ac.in>
2022-07-30 07:53:27 +0530

    Time: 11 ms (93.13%), Space: 7.2 MB (81.43%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -5,8 +5,8 @@ public:
         vector<int> dp(amount+1, 0);
         dp[0] = 1;
         for(int i = 0;i<n;i++){
-            for(int j = 1;j<=amount;j++){
-                if(j-coins[i]>=0 && dp[j-coins[i]]>0){
+            for(int j = coins[i];j<=amount;j++){
+                if(dp[j-coins[i]]>0){
                     dp[j] += dp[j-coins[i]];
                 }
             }
