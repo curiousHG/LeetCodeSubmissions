922156cbde60cc0a9b419a2edff321bf9313692b
Harshit Gupta <me2190885@iitd.ac.in>
2022-08-07 00:44:16 +0530

    Time: 3 ms (82.69%), Space: 7.7 MB (78.34%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int i = nums.size()-1;
-        while(i>0&&nums[i-1]>=nums[i])i--;
-        if(i==0){
-            reverse(nums.begin(),nums.end());
+    void solve(int i, vector<int> &ind, vector<vector<int>> &ans){
+        if(i==ind.size()){
+            ans.push_back(ind);
             return;
         }
-        int j = i;
-        int mini = nums[j];
-        int ind = j;
-        for(int k = j;k<nums.size();k++){
-            if(nums[k]>nums[j-1]){
-                mini = min(mini,nums[k]);
-                ind = k;
-            }
+        for(int j = i;j<ind.size();j++){
+            swap(ind[i],ind[j]);
+            solve(i+1,ind, ans);
+            swap(ind[i],ind[j]);
         }
-        swap(nums[j-1],nums[ind]);
-        sort(nums.begin()+j,nums.end());
-        return;
     }
     vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
         vector<int> ind;
-        vector<vector<int>>ans;
-        for(int i =0;i<nums.size();i++)ind.push_back(i);
-        int per = 1;
-        for(int i=1;i<=nums.size();i++)per=per*i;
-        for(int i = 0;i<per;i++){
-            vector<int> temp;
-            for(int j = 0;j<ind.size();j++){
-                temp.push_back(nums[ind[j]]);
+        for(int i = 0;i<n;i++)ind.push_back(i);
+        solve(0,ind,ans);
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<ans.size();j++){
+                ans[j][i] = nums[ans[j][i]];
             }
-            nextPermutation(ind);
-            ans.push_back(temp);
         }
         return ans;
     }
-    
 };
