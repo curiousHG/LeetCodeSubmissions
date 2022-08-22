d0557fab771ff0ebc5a794ac071b50cdb79a908e
Harshit Gupta <me2190885@iitd.ac.in>
2022-08-22 11:19:06 +0530

    Time: 23 ms (80.79%), Space: 26.4 MB (25.12%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -15,17 +15,17 @@ public:
         unordered_map<int,int> m;
         for(int i = 0;i<in.size();i++)m[in[i]]=i;
         
-        return solve(pre, in , 0, pre.size()-1,0, pre.size()-1,m); 
+        return solve(pre, in , 0, pre.size()-1,0,m); 
     }
-    TreeNode* solve(vector<int>& pre, vector<int>& in, int i, int j, int l , int r,unordered_map<int,int> &m){
+    TreeNode* solve(vector<int>& pre, vector<int>& in, int i, int j, int l,unordered_map<int,int> &m){
         if(i>j)return NULL;
         
         TreeNode* head = new TreeNode(pre[i]);
         // cout<<pre[i]<<" "<<i<<" "<<j<<" "<<l<<" "<<r<<endl;
         
         int p = m[pre[i]];
-        head->left = solve(pre, in, i+1, i+p-l, l, p-1,m);
-        head->right = solve(pre, in,i+p-l+1, j, p+1, r,m);
+        head->left = solve(pre, in, i+1, i+p-l, l,m);
+        head->right = solve(pre, in,i+1+p-l, j, p+1,m);
         return head;
     }
 };
