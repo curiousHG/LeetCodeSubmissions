d49bc976f0e083e030dd4fc73a16bc29ecea3629
Harshit Gupta <me2190885@iitd.ac.in>
2022-08-25 21:05:08 +0530

    Time: 22 ms (95.47%), Space: 13.6 MB (87.69%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<bool> vis;
-    void mark(int j, vector<vector<int>>& isC){
+    void mark(int j, vector<vector<int>>& isC,vector<bool> &vis){
         if(vis[j])return;
         vis[j] = true;
         for(int i = 0;i<isC.size();i++){
-            if(isC[j][i])mark(i, isC);
+            if(isC[j][i])mark(i, isC,vis);
         }
     }
     int findCircleNum(vector<vector<int>>& isC) {
         int n = isC.size(),ans = 0;
-        vis.resize(n, false);
+        vector<bool> vis(n, false);
         for(int i = 0;i<n;i++){
             if(!vis[i]){
                 ans++;
-                mark(i, isC);
+                mark(i, isC,vis);
             }
         }
         return ans;
