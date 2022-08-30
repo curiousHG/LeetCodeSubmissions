5f19d96c0b20e7bb04df2514a840930b01e79b38
Harshit Gupta <me2190885@iitd.ac.in>
2022-08-30 23:27:12 +0530

    Time: 5 ms (91.69%), Space: 6.5 MB (87.11%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,28 +1,60 @@
+// class Solution {
+// public:
+//     int ans = 0;
+//     int mod = 1e9+7;
+//     vector<int> dx = {1,0,0,-1}, dy = {0,1,-1,0}; 
+//     int m,n;
+//     vector<vector<vector<int>>> memo;
+//     int dp(int i, int j, int k){
+//         if(k<0)return 0;
+//         if(check(i,j))return 1;
+//         if(memo[i][j][k]!=-1)return memo[i][j][k];
+//         int val = 0;
+//         for(int l = 0;l<4;l++){
+//             val = (val+dp(i+dx[l], j+dy[l],k-1))%mod;
+//         }
+//         return memo[i][j][k] = val;      
+//     }
+//     bool check(int i, int j){
+//         if(i<0 || j<0 || i>=m || j>=n)return true;
+//         return false;
+//     }
+//     int findPaths(int a, int b, int mm, int R, int C) {
+//         memo.resize(a, vector<vector<int>>(b, vector<int>(mm+1,-1)));
+//         m = a;
+//         n = b;
+//         return dp(R, C, mm);
+//     }
+// };
+
 class Solution {
 public:
-    int ans = 0;
-    int mod = 1e9+7;
-    vector<int> dx = {1,0,0,-1}, dy = {0,1,-1,0}; 
-    int m,n;
-    vector<vector<vector<int>>> memo;
-    int dp(int i, int j, int k){
-        if(k<0)return 0;
-        if(check(i,j))return 1;
-        if(memo[i][j][k]!=-1)return memo[i][j][k];
-        int val = 0;
-        for(int l = 0;l<4;l++){
-            val = (val+dp(i+dx[l], j+dy[l],k-1))%mod;
+    int dp[52][52][52];
+    const int mod = 1e9+7;
+    int func(int i,int j,int m,int n,int moves,int maxMoves)
+    {
+        if(i<0 || j<0 || i>=m || j>=n)
+        {
+            return 1;
         }
-        return memo[i][j][k] = val;      
+        if(moves==maxMoves)
+        {
+            return 0;
+        }
+        if(dp[i][j][moves]!=-1)
+        {
+            return dp[i][j][moves];
+        }
+        int val = 0;
+            
+        val = (val+func(i+1,j,m,n,moves+1,maxMoves))%mod;
+        val = (val+func(i,j+1,m,n,moves+1,maxMoves))%mod;
+        val = (val+func(i-1,j,m,n,moves+1,maxMoves))%mod;
+        val = (val+func(i,j-1,m,n,moves+1,maxMoves))%mod;
+        return dp[i][j][moves] = val;
     }
-    bool check(int i, int j){
-        if(i<0 || j<0 || i>=m || j>=n)return true;
-        return false;
-    }
-    int findPaths(int a, int b, int mm, int R, int C) {
-        memo.resize(a, vector<vector<int>>(b, vector<int>(mm+1,-1)));
-        m = a;
-        n = b;
-        return dp(R, C, mm);
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        memset(dp,-1,sizeof(dp));
+        return func(startRow,startColumn,m,n,0,maxMove);
     }
 };
