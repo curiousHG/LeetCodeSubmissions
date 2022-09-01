42d80e1351c04fb18c3a776efbd1bfa61960b200
Harshit Gupta <me2190885@iitd.ac.in>
2022-09-01 12:27:28 +0530

    Time: 484 ms (53.56%), Space: 16.4 MB (63.28%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -4,12 +4,11 @@ public:
         sort(nums.begin(),nums.end());
         int diff = INT_MAX,n = nums.size(),ans;
         for(int i = 0;i+2<n;i++){
-            if(i>0 && nums[i-1]==nums[i])continue;
+            // if(i>0 && nums[i-1]==nums[i])continue;
             int l= i+1, r = n-1,sum;
             while(l<r){
                 sum = nums[i]+nums[l]+nums[r];
                 if(abs(sum-target)<diff){
-                    
                     diff = abs(sum-target);
                     ans= sum;
                 }
