d5b65c0718af35a4de3b01370142896ca16b5adb
Harshit Gupta <me2190885@iitd.ac.in>
2022-09-01 12:40:06 +0530

    Time: 486 ms (35.71%), Space: 60.5 MB (84.42%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        sort(points.begin(), points.end(), [](auto& a, auto& b){
+            return a[0]*a[0]+a[1]*a[1]<b[0]*b[0]+b[1]*b[1];
+        });
+        vector<vector<int>> ans;
+        for(int i = 0;i<k;i++){
+            ans.push_back(points[i]);
+        }
+        return ans;
+        
+    }
+};
