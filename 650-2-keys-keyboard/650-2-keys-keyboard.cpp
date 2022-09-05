b771d975580a224742b268bdc2b3c6f80034aa0f
Harshit Gupta <me2190885@iitd.ac.in>
2022-09-05 21:39:55 +0530

    Time: 23 ms (51.54%), Space: 6.6 MB (41.57%) - LeetHub

---
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int> dp(n+1,INT_MAX);
+        dp[1] = 0;
+        for(int i = 1;i<=n;i++){
+            for(int j = 2;j*i<=n;j++){
+                dp[j*i] = min(dp[j*i],dp[i]+(j));
+            }
+            cout<<dp[i]<<" ";
+        }
+        return dp[n];
+    }
+};
