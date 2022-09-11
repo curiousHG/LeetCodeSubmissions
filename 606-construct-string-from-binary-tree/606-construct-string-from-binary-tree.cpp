0f822bf6ab2597c52a44cad6bc73b890865d9e5a
Harshit Gupta <me2190885@iitd.ac.in>
2022-09-11 23:11:29 +0530

    Time: 76 ms (10.06%), Space: 66.6 MB (7.73%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if(!root)return "";
+        string ans = to_string(root->val);
+        string l = tree2str(root->left);
+        string r = tree2str(root->right);
+        if(l!="")ans+="("+l+")";
+        if(l=="" && r!="")ans+="("+l+")";
+        if(r!="")ans+="("+r+")";
+        
+        return ans;
+    }
+};
