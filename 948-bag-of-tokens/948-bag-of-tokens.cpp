e0483506e40cc048ca4405814416eb350d4dee9a
Harshit Gupta <me2190885@iitd.ac.in>
2022-09-12 18:09:30 +0530

    Time: 4 ms (90.80%), Space: 10.5 MB (66.86%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -5,14 +5,10 @@ public:
         int n = to.size(),i = 0,j = n-1,sc = 0, maxi = 0;
         while(i<=j){
             if(to[i]<=pow){
-                pow-=to[i];
-                sc++;
-                maxi = max(maxi,sc);
-                i++;
-            }else if(sc>0){
-                sc--;
-                pow+=to[j];
-                j--;
+                pow-=to[i++];
+                maxi = max(maxi,++sc);
+            }else if(sc-->0){
+                pow+=to[j--];
             }else break;
         }
         return maxi;
