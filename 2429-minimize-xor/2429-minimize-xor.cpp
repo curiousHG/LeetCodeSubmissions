3f582860e25501d588a0e6ce0dc7f2a15c0d1ad5
Harshit Gupta <me2190885@iitd.ac.in>
2022-10-06 18:02:10 +0530

    Time: 3 ms (53.68%), Space: 6 MB (52.66%) - LeetHub

--- 2429-minimize-xor/2429-minimize-xor.cpp
+++ 2429-minimize-xor/2429-minimize-xor.cpp
@@ -4,28 +4,23 @@ public:
         int a = __builtin_popcount(num1);
         int b = __builtin_popcount(num2);
         if(a==b)return num1;
-        if(a>b){
-            int ans = num1;
-            int k = a-b;
-            for(int i = 0;i<32;i++){
-                if(k==0)break;
+        int ans = num1;
+        int k = abs(a-b);
+        for(int i = 0;i<32;i++){
+            if(k==0)break;
+            if(a>b){
                 if(num1 & 1<<i){
                     ans -=1<<i;
                     k--;
                 }
+            }else{
+                if(!(num1 & 1<<i)){
+                    ans +=1<<i;
+                    k--;
+                }
             }
-            return ans;
-        }
-        int ans = num1;
-        int k = b-a;
-        for(int i = 0;i<32;i++){
-            if(k==0)break;
-            if(!(num1 & 1<<i)){
-                ans +=1<<i;
-                k--;
-            }
+            
         }
         return ans;
-        
     }
 };
