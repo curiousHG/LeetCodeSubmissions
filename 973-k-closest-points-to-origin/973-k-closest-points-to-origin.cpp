297fa085dbb10a74ea0a246a509f129d7b439fb4
Harshit Gupta <me2190885@iitd.ac.in>
2022-10-08 11:58:16 +0530

    Time: 403 ms (64.15%), Space: 64 MB (67.96%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,19 +1,30 @@
-static const auto init = []{
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
-    return false;
-}();
+// static const auto init = []{
+//     cin.tie(NULL);
+//     ios::sync_with_stdio(false);
+//     return false;
+// }();
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        sort(points.begin(), points.end(), [](const auto& a, const auto& b){
-            return a[0]*a[0]+a[1]*a[1]<b[0]*b[0]+b[1]*b[1];
-        });
+    vector<vector<int>> kClosest(vector<vector<int>>& po, int k) {
+        // sort(points.begin(), points.end(), [](const auto& a, const auto& b){
+        //     return a[0]*a[0]+a[1]*a[1]<b[0]*b[0]+b[1]*b[1];
+        // });
         vector<vector<int>> ans;
+        // for(int i = 0;i<k;i++){
+        //     ans.push_back(points[i]);
+        // }
+        // return ans;
+        int n = po.size();
+        priority_queue<pair<int,int>> pq;
+        for(int i = 0;i<n;i++){
+            int x = po[i][0],y = po[i][1];
+            int dis = x*x + y*y;
+            pq.push({-dis, i});
+        }
         for(int i = 0;i<k;i++){
-            ans.push_back(points[i]);
+            ans.push_back(po[pq.top().second]);
+            pq.pop();
         }
         return ans;
-        
     }
 };
