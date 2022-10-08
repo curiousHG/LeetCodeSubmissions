1d04a33693ef624e4387fac2186054e52b15b801
Harshit Gupta <me2190885@iitd.ac.in>
2022-10-08 11:30:24 +0530

    Time: 386 ms (27.35%), Space: 60.5 MB (25.00%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
@@ -3,14 +3,12 @@ class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(),m = nums2.size();
-        ll ans = 0;
+        int ans = 0;
         if(m%2)for(int i = 0;i<n;i++){
             ans^=nums1[i];
         }
-        for(int i = 0;i<m;i++){
-            if(n%2){
-                ans^=nums2[i];
-            }
+        if(n%2)for(int i = 0;i<m;i++){
+            ans^=nums2[i];
         }
         return ans;
     }
