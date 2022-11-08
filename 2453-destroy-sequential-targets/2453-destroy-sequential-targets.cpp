8764aa22f358d442da6a0e874aaaaae6dc75859b
Harshit Gupta <me2190885@iitd.ac.in>
2022-11-09 03:16:00 +0530

    Time: 776 ms (5.02%), Space: 78.9 MB (9.42%) - LeetHub

---
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int destroyTargets(vector<int>& nums, int space) {
+        unordered_map<int, vector<int>> mp;
+        int maxi = 0, im = 0;
+        for(int i:nums){
+            mp[i%space].push_back(i);
+        }
+        int m = 0,ans = INT_MAX;
+        for(auto it = mp.begin();it!=mp.end();it++){
+            int v = (*it).first;
+            vector<int> k = (*it).second;
+            cout<<v<<" "<<k.size()<<endl;
+            if(k.size()>m){
+                ans = INT_MAX;
+            }
+            if(k.size()>=m){
+                m = k.size();
+                for(int i:k)ans = min(ans, i);
+            }
+        }
+        return ans;
+    }
+};
